114952ca64e7e9221c8d24f6be20d903175d61ac
hkpatel321 <phet30440@gmail.com>
2024-12-14 13:03:04 +0530

    back to default

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,28 @@
 import React from 'react';
-import { Grid } from '@material-ui/core';  
-import { Routes, Route, Link } from 'react-router-dom';  
+import { Grid } from '@material-ui/core';  // Use Grid instead of Container
+
 import Navbar from './components/Navbar/Navbar.js';
 import Home from './components/Home/Home.js';
+import { Route, Switch } from 'react-router-dom';
 import Auth from './components/Auth/Auth.js';
 import { GoogleOAuthProvider } from '@react-oauth/google';
-import PostDetails from './components/PostDetails/PostDetails.jsx';
 import { Redirect } from 'react-router-dom';
+import PostDetails from './components/PostDetails/PostDetails.jsx';
 
 function App() {
   const user = JSON.parse(localStorage.getItem('profile'));
 
   return (
     <GoogleOAuthProvider clientId="885944597905-3bo5bt83v14mv0gj0ug18o40fnhp15j3.apps.googleusercontent.com">
-      <Grid container spacing={3} justifyContent="center"> 
+      <Grid container spacing={3} justifyContent="center"> {/* Use Grid for layout */}
         <Navbar />
-        <Routes> 
-          <Route path="/" element={<Redirect to="/posts" />} /> 
-          <Route path="/posts" element={<Home />} />
-          <Route path="/posts/search" element={<Home />} />
-          <Route path="/posts/:id" element={<PostDetails />} />
-          <Route path="/auth" element={!user ? <Auth /> : <Redirect to="/posts" />} />
-        </Routes>
+        <Switch>
+          <Route path='/' exact component={() => <Redirect to='/posts' />} />
+          <Route path='/posts' exact component={Home} />
+          <Route path='/posts/search' exact component={Home} />
+          <Route path='/posts/:id' component={PostDetails} />
+          <Route path='/auth' exact component={() => (!user ? <Auth /> : <Redirect to="/posts" />)} />
+        </Switch>
       </Grid>
     </GoogleOAuthProvider>
   );
